a8617ad4f6aff8f8f05861d10c22f422b1b1868a
Liza <elizabeth.klevakina@gmail.com>
2024-07-02 14:39:10 +0300

    test, delete later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Header from "src/components/Header";
 import Hero from "src/components/Hero";
 import Portfolio from "src/components/Portfolio";
 import Services from "src/components/Services";
+import Test from "src/components/Test";
 import Workflow from "src/components/Workflow";
 
 export default function Home() {
   return (
     <main className="mx-7">
+      <Test/>
       <Header/>
       <Hero/>
       <About/>
---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div style={{ backgroundColor: "#E6E4D5" }} className="w-full h-96">
+      <div className="w-28 h-28 bg-black"></div>
+      Full Stack Designer
+    </div>
+  );
+};
+
+export default Test;
