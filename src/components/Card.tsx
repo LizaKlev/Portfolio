7ca35c079428db98cd4b4058650b8662cd9d1d04
Liza <elizabeth.klevakina@gmail.com>
2024-07-09 16:36:55 +0200

    Cards

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = ({
+  name,
+  description,
+  image,
+}: {
+  name: string;
+  description: string;
+  image: string;
+}) => {
+  return (
+    <div className="border border-beige rounded-2xl px-5 pt-5 pb-14 w-2/6">
+      <img src={image} alt={name} />
+      <h2 className="text-white mb-3 text-2xl">{name}</h2>
+      <p className=" text-opacity-80 text-white text-lg">{description}</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card";
 
 const Services = () => {
   return (
-    <div>
-      <h2 className='text-white'>SERVICES I PROVIDE</h2>
+    <div className="px-3 py-14">
+      <h2 className="text-white text-6xl mb-11">SERVICES I PROVIDE</h2>
+      <Card
+        name="DESIGN & DEVELOPMENT"
+        description="From concept to launch, I provide comprehensive design and development services to bring your vision to life"
+        image="D&D1.svg"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
